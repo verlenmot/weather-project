223e69dfed75c40a209104cb27eebfedccae9743
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-08 10:49:39 +0100

    feat: add variable tenant_id

--- config/terraform/variables.tf
+++ config/terraform/variables.tf
@@ -1,5 +1,11 @@
 variable "subscription_id" {
   description = "Subscription ID to be used"
-  default = ""
+  default     = ""
   type        = string
+}
+
+variable "tenant_id" {
+  description = "Tenant ID to be used"
+  default = ""
+  type = string
 }
