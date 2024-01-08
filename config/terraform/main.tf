216a89d773ca703b1480ca015cb18aa7eb0ca4a8
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-08 13:17:18 +0100

    feat: add subnet to virtual network

--- config/terraform/main.tf
+++ config/terraform/main.tf
@@ -28,51 +28,57 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-weather-project"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.55.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
+
+subnet {
+  name = "snet-weather-project"
+  address_prefix = "10.0.0.0/24"
+}
 }
 
-# Storage Accounts & Containers
-resource "azurerm_storage_account" "storage-raw" {
-  name                     = "stweatherprojectraw"
-  location                 = "West Europe"
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  is_hns_enabled = true
-  public_network_access_enabled = false
-}
 
-resource "azurerm_storage_container" "forecast-raw" {
-  name = "forecast"
-  storage_account_name = azurerm_storage_account.storage-raw.name
-}
+# # Storage Accounts & Containers
+# resource "azurerm_storage_account" "storage-raw" {
+#   name                     = "stweatherprojectraw"
+#   location                 = "West Europe"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   account_kind             = "StorageV2"
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+#   is_hns_enabled = true
+#   public_network_access_enabled = false
+# }
 
-resource "azurerm_storage_container" "realtime-raw" {
-  name = "realtime"
-  storage_account_name = azurerm_storage_account.storage-raw.name
-}
+# resource "azurerm_storage_container" "forecast-raw" {
+#   name = "forecast"
+#   storage_account_name = azurerm_storage_account.storage-raw.name
+# }
 
-resource "azurerm_storage_account" "storage-serve" {
-  name                     = "stweatherprojectserve"
-  location                 = "West Europe"
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  is_hns_enabled = true
-  public_network_access_enabled = false
-}
+# resource "azurerm_storage_container" "realtime-raw" {
+#   name = "realtime"
+#   storage_account_name = azurerm_storage_account.storage-raw.name
+# }
 
-resource "azurerm_storage_container" "forecast-serve" {
-  name = "forecast"
-  storage_account_name = azurerm_storage_account.storage-serve.name
-}
+# resource "azurerm_storage_account" "storage-serve" {
+#   name                     = "stweatherprojectserve"
+#   location                 = "West Europe"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   account_kind             = "StorageV2"
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+#   is_hns_enabled = true
+#   public_network_access_enabled = false
+# }
 
-resource "azurerm_storage_container" "realtime-serve" {
-  name = "realtime"
-  storage_account_name = azurerm_storage_account.storage-serve.name
-}
+# resource "azurerm_storage_container" "forecast-serve" {
+#   name = "forecast"
+#   storage_account_name = azurerm_storage_account.storage-serve.name
+# }
+
+# resource "azurerm_storage_container" "realtime-serve" {
+#   name = "realtime"
+#   storage_account_name = azurerm_storage_account.storage-serve.name
+# }
 
 # # Key vault
 # resource "azurerm_key_vault" "keyvault" {
