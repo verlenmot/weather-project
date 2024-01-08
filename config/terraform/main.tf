f879b09f3011a6087c840e1e4853b36af159055e
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-08 10:17:12 +0100

    feat: add storage accounts to terraform

--- config/terraform/main.tf
+++ config/terraform/main.tf
@@ -13,8 +13,7 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-
-
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-weather-project"
   location = "West Europe"
@@ -23,9 +22,31 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-weather-project"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.55.0.0/16"]
 }
+
+# Storage Account & Data Lakes
+
+resource "azurerm_storage_account" "storage-raw" {
+  name                     = "stweatherprojectraw"
+  location                 = "West Europe"
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+resource "azurerm_storage_account" "storage-serve" {
+  name                     = "stweatherprojectserve"
+  location                 = "West Europe"
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
