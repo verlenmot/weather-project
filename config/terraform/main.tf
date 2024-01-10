115da86a45fe00e255c5796d3439021e6d128a44
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-10 20:03:41 +0100

    feat: add storage and key vault interaction

--- config/terraform/main.tf
+++ config/terraform/main.tf
@@ -15,15 +15,16 @@ provider "azurerm" {
   features { 
     key_vault {
       purge_soft_deleted_secrets_on_destroy = true
+      purge_soft_delete_on_destroy = true
     }
   }
 
   subscription_id = var.subscription_id
 }
 
-provider "databricks" {
-  host = azurerm_databricks_workspace.dbw.workspace_url
-}
+# provider "databricks" {
+#   host = azurerm_databricks_workspace.dbw.workspace_url
+# }
 
 data "azurerm_client_config" "current" {
 }
@@ -103,152 +104,180 @@ resource "azurerm_storage_container" "realtime-raw" {
   storage_account_name = azurerm_storage_account.storage-raw.name
 }
 
-# resource "azurerm_storage_account" "storage-serve" {
-#   name                     = "stweatherprojectserve"
-#   location                 = "westeurope"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   account_kind             = "StorageV2"
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
-#   is_hns_enabled           = true
-#   enable_https_traffic_only = true
+resource "azurerm_storage_account" "storage-serve" {
+  name                     = "stweatherprojectserve"
+  location                 = "westeurope"
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  is_hns_enabled           = true
+  enable_https_traffic_only = true
 
-#   network_rules {
-#     default_action = "Deny"
-#     ip_rules       = [var.ip, var.ip2]
-#     bypass         = ["AzureServices"]
-#   }
-# }
+  network_rules {
+    default_action = "Deny"
+    ip_rules       = [var.ip, var.ip2]
+    bypass         = ["AzureServices"]
+  }
+}
 
-# resource "azurerm_storage_container" "forecast-serve" {
-#   name                 = "forecast"
-#   storage_account_name = azurerm_storage_account.storage-serve.name
-# }
+resource "azurerm_storage_container" "forecast-serve" {
+  name                 = "forecast"
+  storage_account_name = azurerm_storage_account.storage-serve.name
+}
 
-# resource "azurerm_storage_container" "realtime-serve" {
-#   name                 = "realtime"
-#   storage_account_name = azurerm_storage_account.storage-serve.name
-# }
+resource "azurerm_storage_container" "realtime-serve" {
+  name                 = "realtime"
+  storage_account_name = azurerm_storage_account.storage-serve.name
+}
 
 # # Key vault
-resource "azurerm_key_vault" "keyvault" {
+resource "azurerm_key_vault" "kv" {
   name                = "kv-weather-project"
   location            = "westeurope"
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "standard"
   tenant_id           = var.tenant_id
-  
+
+    network_acls {
+    default_action = "Deny"
+    ip_rules = [var.ip, var.ip2]
+    bypass   = "AzureServices"
+    
+  }
 }
 
+
 resource "azurerm_key_vault_access_policy" "kv-access-storage" {
-  key_vault_id = azurerm_key_vault.keyvault.id
+  key_vault_id = azurerm_key_vault.kv.id
   tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = data.azurerm_client_config.current.object_id
 
-  secret_permissions = ["Set", "List"]
+  secret_permissions = ["Get", "Set", "List", "Delete", "Purge"]
 }
 
-# resource "azurerm_key_vault_access_policy" "kv-access-db" {
-#   key_vault_id = azurerm_key_vault.keyvault.id
-#   tenant_id = data.azurerm_client_config.current.tenant_id
-#   object_id = data.azurerm_client_config.current.object_id
+resource "azurerm_key_vault_secret" "secret-sas-raw-forecast" {
+  name = "sas-raw-forecast"
+  value = data.azurerm_storage_account_blob_container_sas.sas-raw-forecast.sas
+  key_vault_id = azurerm_key_vault.kv.id
+  depends_on = [azurerm_key_vault_access_policy.kv-access-storage]
+}
 
-#   secret_permissions = ["Get", "List"]
-# }
+resource "azurerm_key_vault_secret" "secret-sas-raw-realtime" {
+  name = "sas-raw-realtime"
+  value = data.azurerm_storage_account_blob_container_sas.sas-raw-realtime.sas
+  key_vault_id = azurerm_key_vault.kv.id
+   depends_on = [azurerm_key_vault_access_policy.kv-access-storage]
+}
 
+resource "azurerm_key_vault_secret" "secret-sas-serve-forecast" {
+  name = "sas-serve-forecast"
+  value = data.azurerm_storage_account_blob_container_sas.sas-serve-forecast.sas
+  key_vault_id = azurerm_key_vault.kv.id
+   depends_on = [azurerm_key_vault_access_policy.kv-access-storage]
+}
+
+resource "azurerm_key_vault_secret" "secret-sas-serve-realtime" {
+  name = "sas-serve-realtime"
+  value = data.azurerm_storage_account_blob_container_sas.sas-serve-realtime.sas
+  key_vault_id = azurerm_key_vault.kv.id
+   depends_on = [azurerm_key_vault_access_policy.kv-access-storage]
+}
 
 # Storage Container Keys
 
-# data "azurerm_storage_account_blob_container_sas" "sas-raw-forecast" {
-#   connection_string = azurerm_storage_account.storage-raw.primary_connection_string
-#   container_name = azurerm_storage_container.forecast-raw.name
+data "azurerm_storage_account_blob_container_sas" "sas-raw-forecast" {
+  connection_string = azurerm_storage_account.storage-raw.primary_connection_string
+  container_name = azurerm_storage_container.forecast-raw.name
 
-#   start = "2024-01-01T00:00:00+0000"
-#   expiry = "2024-12-20T00:00:00+0000"
-
-
-#   permissions {
-#     read = true
-#     write = false # Overwrite content of an existing blob
-#     delete = false # Delete blobs
-#     create = true # Add new blobs
-#     list = true # List blobs
-#     add = false # Append data to blob
-#   }
-# }
-
-# data "azurerm_storage_account_blob_container_sas" "sas-raw-realtime" {
-#   connection_string = azurerm_storage_account.storage-raw.primary_connection_string
-#   container_name = azurerm_storage_container.realtime-raw.name
-
-#   start = "2024-01-01T00:00:00+0000"
-#   expiry = "2024-12-20T00:00:00+0000"
+  start = "2024-01-01T00:00:00+0000"
+  expiry = "2024-12-20T00:00:00+0000"
 
 
-#   permissions {
-#     read = true
-#     write = false # Overwrite content of an existing blob
-#     delete = false # Delete blobs
-#     create = true # Add new blobs
-#     list = true # List blobs
-#     add = false # Append data to blob
-#   }
-# }
+  permissions {
+    read = true
+    write = false # Overwrite content of an existing blob
+    delete = false # Delete blobs
+    create = true # Add new blobs
+    list = true # List blobs
+    add = false # Append data to blob
+  }
+}
 
-# data "azurerm_storage_account_blob_container_sas" "sas-serve-forecast" {
-#   connection_string = azurerm_storage_account.storage-serve.primary_connection_string
-#   container_name = azurerm_storage_container.forecast-serve.name
+data "azurerm_storage_account_blob_container_sas" "sas-raw-realtime" {
+  connection_string = azurerm_storage_account.storage-raw.primary_connection_string
+  container_name = azurerm_storage_container.realtime-raw.name
 
-#   start = "2024-01-01T00:00:00+0000"
-#   expiry = "2024-12-20T00:00:00+0000"
-
-
-#   permissions {
-#     read = true
-#     write = false # Overwrite content of an existing blob
-#     delete = false # Delete blobs
-#     create = true # Add new blobs
-#     list = true # List blobs
-#     add = false # Append data to blob
-#   }
-# }
-
-# data "azurerm_storage_account_blob_container_sas" "sas-serve-realtime" {
-#   connection_string = azurerm_storage_account.storage-serve.primary_connection_string
-#   container_name = azurerm_storage_container.realtime-serve.name
-
-#   start = "2024-01-01T00:00:00+0000"
-#   expiry = "2024-12-20T00:00:00+0000"
+  start = "2024-01-01T00:00:00+0000"
+  expiry = "2024-12-20T00:00:00+0000"
 
 
-#   permissions {
-#     read = true
-#     write = false # Overwrite content of an existing blob
-#     delete = false # Delete blobs
-#     create = true # Add new blobs
-#     list = true # List blobs
-#     add = false # Append data to blob
-#   }
-# }
+  permissions {
+    read = true
+    write = false # Overwrite content of an existing blob
+    delete = false # Delete blobs
+    create = true # Add new blobs
+    list = true # List blobs
+    add = false # Append data to blob
+  }
+}
 
-# #   network_acls {
-# #     default_action = "Deny"
-# #     ip_rules = [var.ip, var.ip2]
-# #     bypass   = "AzureServices"
-    
-# #   }
-# # }
+data "azurerm_storage_account_blob_container_sas" "sas-serve-forecast" {
+  connection_string = azurerm_storage_account.storage-serve.primary_connection_string
+  container_name = azurerm_storage_container.forecast-serve.name
 
-# # # # # Databricks
+  start = "2024-01-01T00:00:00+0000"
+  expiry = "2024-12-20T00:00:00+0000"
+
+
+  permissions {
+    read = true
+    write = false # Overwrite content of an existing blob
+    delete = false # Delete blobs
+    create = true # Add new blobs
+    list = true # List blobs
+    add = false # Append data to blob
+  }
+}
+
+data "azurerm_storage_account_blob_container_sas" "sas-serve-realtime" {
+  connection_string = azurerm_storage_account.storage-serve.primary_connection_string
+  container_name = azurerm_storage_container.realtime-serve.name
+
+  start = "2024-01-01T00:00:00+0000"
+  expiry = "2024-12-20T00:00:00+0000"
+
+
+  permissions {
+    read = true
+    write = false # Overwrite content of an existing blob
+    delete = false # Delete blobs
+    create = true # Add new blobs
+    list = true # List blobs
+    add = false # Append data to blob
+  }
+}
+
+# # # # Databricks
 
 # resource "azurerm_databricks_workspace" "dbw" {
 #   name                        = "dbw-weather-project"
 #   location                    = "westeurope"
 #   resource_group_name         = azurerm_resource_group.rg.name
-#   sku                         = "standard"
+#   sku                         = "premium"
 #   managed_resource_group_name = "rg-managed-weather-project"
 # }
 
+# resource "databricks_secret_scope" "db-secret-scope" {
+#   name = "secret-scope-weather-project"
+
+#   keyvault_metadata {
+#     resource_id = azurerm_key_vault.kv.id
+#     dns_name    = azurerm_key_vault.kv.vault_uri
+#   }
+
+#    depends_on = [azurerm_key_vault_access_policy.kv-access-storage]
+# }
 
 
 
