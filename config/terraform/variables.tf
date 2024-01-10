2ffe19f097b7eaa6a8ebafcd7d5809d3408e9e6c
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-10 20:10:45 +0100

    feat: replace reference to id with azurem client config

--- config/terraform/variables.tf
+++ config/terraform/variables.tf
@@ -1,21 +1,9 @@
-variable "subscription_id" {
-  description = "Subscription ID to be used"
-  default     = ""
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Tenant ID to be used"
-  default     = ""
-  type        = string
-}
-
 variable "ip" {
   default = ""
 }
 
 variable "ip2" {
-  default = ""  
+  default = ""
 }
 
 variable "alert-email" {
