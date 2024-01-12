770a1431e44d5c19a74a4d7829fe8509d6ee7760
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-12 09:52:13 +0100

    feat: remove serve storage

--- config/terraform/main.tf
+++ config/terraform/main.tf
@@ -112,34 +112,6 @@ resource "azurerm_resource_group" "rg" {
 #   storage_account_name = azurerm_storage_account.storage-raw.name
 # }
 
-# resource "azurerm_storage_account" "storage-serve" {
-#   name                      = "stweatherprojectserve"
-#   location                  = "westeurope"
-#   resource_group_name       = azurerm_resource_group.rg.name
-#   account_kind              = "StorageV2"
-#   account_tier              = "Standard"
-#   account_replication_type  = "GRS"
-#   is_hns_enabled            = true
-#   enable_https_traffic_only = true
-
-#   network_rules {
-#     default_action = "Deny"
-#     ip_rules       = [var.ip, var.ip2]
-#     bypass         = ["AzureServices"]
-#   }
-# }
-
-# resource "azurerm_storage_container" "forecast-serve" {
-#   name                 = "forecast"
-#   storage_account_name = azurerm_storage_account.storage-serve.name
-# }
-
-# resource "azurerm_storage_container" "realtime-serve" {
-#   name                 = "realtime"
-#   storage_account_name = azurerm_storage_account.storage-serve.name
-# }
-# ## Storage Container Keys
-
 # data "azurerm_storage_account_blob_container_sas" "sas-raw-forecast" {
 #   connection_string = azurerm_storage_account.storage-raw.primary_connection_string
 #   container_name    = azurerm_storage_container.forecast-raw.name
@@ -173,42 +145,6 @@ resource "azurerm_resource_group" "rg" {
 #     create = true  # Add new blobs
 #     list   = true  # List blobs
 #     add    = false # Append data to blob
-#   }
-# }
-
-# data "azurerm_storage_account_blob_container_sas" "sas-serve-forecast" {
-#   connection_string = azurerm_storage_account.storage-serve.primary_connection_string
-#   container_name    = azurerm_storage_container.forecast-serve.name
-
-#   start  = "2024-01-01T00:00:00+0000"
-#   expiry = "2024-12-20T00:00:00+0000"
-
-
-#   permissions {
-#     read   = false
-#     write  = true  # Overwrite content of an existing blob
-#     delete = false # Delete blobs
-#     create = true  # Add new blobs
-#     list   = true  # List blobs
-#     add    = true  # Append data to blob
-#   }
-# }
-
-# data "azurerm_storage_account_blob_container_sas" "sas-serve-realtime" {
-#   connection_string = azurerm_storage_account.storage-serve.primary_connection_string
-#   container_name    = azurerm_storage_container.realtime-serve.name
-
-#   start  = "2024-01-01T00:00:00+0000"
-#   expiry = "2024-12-20T00:00:00+0000"
-
-
-#   permissions {
-#     read   = false
-#     write  = true  # Overwrite content of an existing blob
-#     delete = false # Delete blobs
-#     create = true  # Add new blobs
-#     list   = true  # List blobs
-#     add    = true  # Append data to blob
 #   }
 # }
 
