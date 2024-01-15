5bad048e4f06f9dcb5abafab57cc1ee7f6124aeb
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-15 16:59:40 +0100

    feat: integrate objects into main

--- scala/pipeline_foreceast/src/main/scala/forecast/Main.scala
+++ scala/pipeline_foreceast/src/main/scala/forecast/Main.scala
@@ -1,18 +1,27 @@
 package forecast
+
+import forecast.processing.DataframeFlattener
 import ingestion.ApiRequest
 import ingestion.ErrorHandler
-import ingestion.DataFrameCreator
+import ingestion.DataframeLoader
+import processing.DataframeEnricher
 
 
 object Main {
   def main(args: Array[String]): Unit = {
+    // Ingestion
+
     // val apiKey: String = dbutils.secrets.get(scope="scope-weather", key="api") -> Databricks
     // Temporary dev config
-    val apiData: Map[String, Any] = ApiRequest.ApiConnectionForecast("amsterdam", devConfig.apiKey)
-//    val apiData: Map[String, Any] = devConfig.testData
+//    val apiData: Map[String, Any] = ApiRequest.ApiConnectionForecast("amsterdam", devConfig.apiKey)
+    val apiData: Map[String, Any] = devConfig.mockData
 //    ErrorHandler.flow(apiData("statusCode").asInstanceOf[Int])
+    val nestedDataframe = DataframeLoader.loadDataFrame(apiData("data").asInstanceOf[String])
 
-    println(apiData)
-//    DataFrameCreator.df.show()
+    // Process Step - Enrichment
+    val enrichedTimeDataframe = DataframeEnricher.requestTimeAdd(nestedDataframe, apiData("requestDateTime").asInstanceOf[Seq[String]](0))
+
+    enrichedTimeDataframe.show()
+
   }
 }
