9cc8dca6eeb213cc79e7f4a52d3206d888fdbc69
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-15 11:11:52 +0100

    feat: add api connection code

--- scala/pipeline_foreceast/src/main/scala/forecast/Main.scala
+++ scala/pipeline_foreceast/src/main/scala/forecast/Main.scala
@@ -1,5 +1,12 @@
 package forecast
+import ingestion.ApiRequest
+
 
 object Main {
-
+  def main(args: Array[String]): Unit = {
+    // val apiKey: String = dbutils.secrets.get(scope="scope-weather", key="api") -> Databricks
+    // Temporary dev config
+    val apiData: Map[String, Any] = ApiRequest.ApiConnectionForecast("amsterdam", devConfig.apiKey)
+    println(apiData)
+  }
 }
---
+++ scala/pipeline_foreceast/src/main/scala/forecast/ingestion/ApiRequest.scala
@@ -0,0 +1,27 @@
+package forecast.ingestion
+
+object ApiRequest {
+  def ApiConnectionForecast(location: String, apiKey: String): Map[String, Any] = {
+    val paramMap: Map[String, String] = Map(
+      "location" -> location,
+      "apikey" -> apiKey,
+      "units" -> "metric",
+      "timesteps" -> "1d"
+    )
+
+    val r: requests.Response = requests.get("https://api.tomorrow.io/v4/weather/forecast", params = paramMap)
+    val text: String = r.text()
+    val statusCode: Int = r.statusCode
+    val statusMessage: String = r.statusMessage
+    val headers: Map[String, Seq[String]] = r.headers
+
+    val ApiOutput: Map[String, Any] = Map(
+      "statusCode" -> statusCode,
+      "statusMessage" -> statusMessage,
+      "data" -> text,
+      "metadata" -> headers
+    )
+
+    return ApiOutput
+  }
+}
