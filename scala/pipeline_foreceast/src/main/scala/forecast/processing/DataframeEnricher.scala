96fa356df896b83e5a10398a1287aca2261288f6
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-15 16:58:58 +0100

    feat: add DataframeEnricher with request timestamp

---
+++ scala/pipeline_foreceast/src/main/scala/forecast/processing/DataframeEnricher.scala
@@ -0,0 +1,16 @@
+package forecast.processing
+
+object DataframeEnricher {
+
+  import org.apache.spark.sql.DataFrame
+  import org.apache.spark.sql.functions._
+
+  def requestTimeAdd(dataFrame: DataFrame, requestTime: String): DataFrame = {
+
+    val adjustedTime: String = requestTime.drop(5)
+
+    val enrichedDataFrame: DataFrame = dataFrame.withColumn("request_timestamp", to_timestamp(lit(adjustedTime), "dd MMM yyyy HH:mm:ss z"))
+    return enrichedDataFrame
+//    "Mon, 15 Jan 2024 11:04:51 GMT"
+  }
+}
