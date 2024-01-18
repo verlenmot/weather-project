4ef4afa55cbe618ff180fcc3c9207e0c3dcac177
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-18 22:24:36 +0100

    feat: integrate modules into main

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,8 +82,9 @@ module "keyvault" {
   subnet_ids       = [module.vnet.private_subnet_id, module.vnet.public_subnet_id]
 
   secrets = {
-    sas = module.storage.sas_storage
-    api = var.api_key
+    sas     = module.storage.sas_storage
+    api     = var.api_key
+    storage = module.storage.storage_name
   }
 }
 
@@ -96,7 +97,6 @@ module "vnet" {
 }
 
 # Databricks 
-
 resource "azurerm_databricks_workspace" "dbw" {
   name                        = "dbw-${var.project_name}-${random_id.instance.hex}"
   resource_group_name         = azurerm_resource_group.rg.name
@@ -116,21 +116,19 @@ resource "azurerm_databricks_workspace" "dbw" {
   depends_on = [module.vnet]
 }
 
-
-# module "budget_db" {
-#   source       = "./modules/azure/budget"
-#   rg_id        = azurerm_databricks_workspace.dbw.managed_resource_group_id
-#   alert_email  = var.alert_email
-#   amount_array = [10, 30]
-# }
+module "budget_db" {
+  source       = "./modules/azure/budget"
+  rg_id        = azurerm_databricks_workspace.dbw.managed_resource_group_id
+  alert_email  = var.alert_email
+  amount_array = [10, 30]
+}
 
 module "setup" {
-  source       = "./modules/databricks/setup"
-  project_name = var.project_name
-  secret_kv    = module.keyvault.kv
-  # notebooks = {
-  #   development         = """
-  # }
+  source          = "./modules/databricks/setup"
+  project_name    = var.project_name
+  secret_kv       = module.keyvault.kv
+  forecast_source = var.forecast_source
+  realtime_source = var.realtime_source
 }
 
 module "compute" {
@@ -138,8 +136,30 @@ module "compute" {
   project_name = var.project_name
 }
 
-# module "visualisation" {
-#   source       = "./modules/databricks/visualisation"
-#   project_name = var.project_name
-#   directory    = module.setup.directory
-# }
+module "job" {
+  source           = "./modules/databricks/job"
+  project_name     = var.project_name
+  project_instance = random_id.instance.hex
+  dev_email        = var.alert_email
+  warehouse_id     = module.compute.warehouse.id
+  query_map        = module.query.query_map
+  pool_id          = module.compute.pool_id
+  dashboard_id     = module.visualisation.dashboard_id
+  city             = var.city
+  depends_on       = [module.setup, module.compute, module.query]
+}
+
+module "query" {
+  source           = "./modules/databricks/query"
+  warehouse_id     = module.compute.warehouse.data_source_id
+  workspace_folder = module.setup.directory.object_id
+
+}
+
+module "visualisation" {
+  source       = "./modules/databricks/visualisation"
+  project_name = var.project_name
+  directory    = module.setup.directory
+  query_map    = module.query.query_map
+  depends_on   = [module.query]
+}
