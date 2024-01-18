dd68f1aa2a66ae9dbce4111703ffccbbf1f1ee2e
Lennert Vermote <lennert.vermote@linkit.nl>
2024-01-18 22:20:13 +0100

    feat: add realtime and forecast jobs

--- terraform/modules/databricks/job/variables.tf
+++ terraform/modules/databricks/job/variables.tf
@@ -7,3 +7,23 @@ variable "project_instance" {
 variable "dev_email" {
 
 }
+
+variable "warehouse_id" {
+
+}
+
+variable "query_map" {
+  type = map(string)
+}
+
+variable "pool_id" {
+
+}
+
+variable "city" {
+
+}
+
+variable "dashboard_id" {
+
+}
